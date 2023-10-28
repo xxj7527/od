89c92f3cc2acdc98d5c3c57673f2ee57c4991e5b
superen7527 <45636863+superen7527@users.noreply.github.com>
2023-10-28 16:56:11 +0800

    Add files via upload

--- config/site.config.js
+++ config/site.config.js
@@ -7,24 +7,21 @@ module.exports = {
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your Vercel's environment variable 'NEXT_PUBLIC_USER_PRINCIPLE_NAME' if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'spencer@spwoo.onmicrosoft.com',
+  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'mainod@superen7527.onmicrosoft.com',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
   icon: '/icons/128.png',
 
-  // Prefix for KV Storage
-  kvPrefix: process.env.KV_PREFIX || '',
-
   // The name of your website. Present alongside your icon.
-  title: "Spencer's OneDrive",
+  title: "Super's OneDrive!",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
-  maxItems: 100,
+  maxItems: 200,
 
   // [OPTIONAL] We use Google Fonts natively for font customisations.
   // You can check and generate the required links and names at https://fonts.google.com.
@@ -37,24 +34,23 @@ module.exports = {
 
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
-  footer:
-    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by SpencerWoo.',
+  footer:'<a href="https://github.com/superen7527/od/edit/main/config/site.config.js" target="_blank">Super\'s OneDrive!</a>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ["/私密","/备份/配置文件_激活码"],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:spencer.wushangbo@gmail.com',
+  email: 'mailto:gustesalome@gmail.com',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
-    },
+      name:'',//必须要name不然报错Type error: Argument of type '(l: {    name: string;    link: string;}) => JSX.Element' is not assignable to parameter of type '(value: { link: string; }, index: number, array: { link: string; }[]) => Element'.
+      link:'https://github.com/superen7527/od/blob/main/config/site.config.js',//没有name原来不会有位置啊难怪空字符也不行
+    }
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
