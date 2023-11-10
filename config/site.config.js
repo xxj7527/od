ed2919a8b0cf80d0597a50a467fcf2535e3cc0e0
xxj7527 <86597262+xxj7527@users.noreply.github.com>
2023-11-10 17:49:05 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -1,60 +1,81 @@
 /**
+
  * This file contains the configuration used for customising the website, such as the folder to share,
  * the title, used Google fonts, site icons, contact info, etc.
+
  */
+
 module.exports = {
+
   // This is what we use to identify who you are when you are initialising the website for the first time.
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your Vercel's environment variable 'NEXT_PUBLIC_USER_PRINCIPLE_NAME' if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'mainod@superen7527.onmicrosoft.com',
+
+  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'lxying@superen7527.onmicrosoft.com',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/icons/gg.png',
+
+  icon: '/icons/mm.png',//'https://q1.qlogo.cn/g?b=qq&nk=*&s=640',
+
+  // Prefix for KV Storage
+
+  kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Super's OneDrive!",
+
+  title: "lxying的网盘",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/Public',
+
+  baseDirectory: '/网站上的',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
+
   maxItems: 200,
 
   // [OPTIONAL] We use Google Fonts natively for font customisations.
   // You can check and generate the required links and names at https://fonts.google.com.
   // googleFontSans - the sans serif font used in onedrive-vercel-index.
+
   googleFontSans: 'Inter',
+
   // googleFontMono - the monospace font used in onedrive-vercel-index.
+
   googleFontMono: 'Fira Mono',
+
   // googleFontLinks -  an array of links for referencing the google font assets.
+
   googleFontLinks: ['https://fonts.googleapis.com/css2?family=Fira+Mono&family=Inter:wght@400;500;700&display=swap'],
 
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
-  footer:'<a href="https://github.com/superen7527/od/edit/main/config/site.config.js" target="_blank">Super\'s OneDrive!</a>',
+
+  footer: '<a ' /*+ 'href="https://github.com/xxj7527/onedrive-vercel-index/edit/main/config/site.config.js" ' */+ 'target="_blank">Lxying\'s OneDrive ~ ❤</a>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ["/私密","/备份/配置文件_激活码"],
+
+  protectedRoutes: ['/文档/个人','/private'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:gustesalome@gmail.com',
+
+  email: 'mailto:lxying@zjnu.edu.cn',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
+
   links: [
-    {
-      name:'',//必须要name不然报错Type error: Argument of type '(l: {    name: string;    link: string;}) => JSX.Element' is not assignable to parameter of type '(value: { link: string; }, index: number, array: { link: string; }[]) => Element'.
-      link:'https://github.com/superen7527/od/blob/main/config/site.config.js',//没有name原来不会有位置啊难怪空字符也不行
-    }
+
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
   // without timezone and replacing T with space.
+
   datetimeFormat: 'YYYY-MM-DD HH:mm:ss',
+
 }
